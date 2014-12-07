e885da95ad47e57d516ba503715c2a0d6a55f115
Kurnia D Win <q.dwi2nt@gmail.com>
2014-12-07 20:16:56 +0700

    tambah desktop

---
+++ desktop/src/com/tekmob/spaceexplorer/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.tekmob.spaceexplorer;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tekmob.spaceexplorer.SpaceExplorer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 480;
+		new LwjglApplication(new SpaceExplorer(), config);
+	}
+}
