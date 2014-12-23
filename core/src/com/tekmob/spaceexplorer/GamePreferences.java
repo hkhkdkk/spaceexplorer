2a5f0d5d7bf1ad4c7d48ca5bf9f08c947cc36b26
Rahmat Rasyidi Hakim <rahmad.rasyidi@ui.ac.id>
2014-12-23 17:23:13 +0700

    minus onback game, score sama milestone belum di set, missile msh ada masalah

--- core/src/com/tekmob/spaceexplorer/GamePreferences.java
+++ core/src/com/tekmob/spaceexplorer/GamePreferences.java
@@ -17,16 +17,43 @@ public class GamePreferences {
     public GamePreferences(){
     }
 
-    public void loadSetting(){
-        isHowToPlayEnabled();
-    }
-
     protected Preferences getPrefs(){
         return Gdx.app.getPreferences(PREFS_NAME);
     }
 
     public boolean isHowToPlayEnabled(){
         return getPrefs().getBoolean(PREF_HOWTOPLAY_ENABLED, true);
+    }
+
+    public float getVolume()
+    {
+        return getPrefs().getFloat( PREF_VOLUME, 0.5f );
+    }
+
+    public void setVolume(
+            float volume )
+    {
+        getPrefs().putFloat( PREF_VOLUME, volume );
+        getPrefs().flush();
+    }
+
+    public boolean isSoundEnabled(){
+        return getPrefs().getBoolean(PREF_SOUND_ENABLED, true);
+    }
+
+    public void setSoundEnabled(boolean how){
+        getPrefs().putBoolean(PREF_SOUND_ENABLED, how);
+        getPrefs().flush();
+    }
+
+
+    public boolean isMusicEnabled(){
+        return getPrefs().getBoolean(PREF_MUSIC_ENABLED, true);
+    }
+
+    public void setMusicEnabled(boolean how){
+        getPrefs().putBoolean(PREF_MUSIC_ENABLED, how);
+        getPrefs().flush();
     }
 
     public void setHowToPlayEnabled(boolean how){
